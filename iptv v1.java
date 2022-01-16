d14a201cfbd53c53f420d7433503cabd997547fc
liadk07 <91607695+liad07@users.noreply.github.com>
2022-01-16 19:54:37 +0200

    Add files via upload

---
+++ iptv v1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class IPTV {
+    //crack apps iptv 1
+    //הוראות הפעלה: פתח את הקובץ README המצורף ובחר ערוץ שבאלך והכנס לתוכנית
+
+        public static void main(String[] args) {
+            Scanner read=new Scanner(System.in);
+            String link;
+            String chanel;
+            link="www.sites.google.com/view/crackapps/iptv2/";
+            String Sum;
+            System.out.println("please enter the chanel");
+            chanel=read.next();
+            System.out.println("the link is: "+(link+chanel));
+
+        }
+    }
+
